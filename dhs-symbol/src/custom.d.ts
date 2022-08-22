04d0373840c2c866f6d68ffb02cb19604cd08e5a
ffrogers <chris@firemapping.com>
2022-08-22 11:20:35 -0700

    Update custom.d.ts

--- dhs-symbol/src/custom.d.ts
+++ dhs-symbol/src/custom.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Dignitas Technologies, LLC
+ *
+ * This file and its contents are governed by one or more distribution and
+ * copyright statements as described in the LICENSE.txt file distributed with
+ * this work.
+ */
+
 //TODO: remove this if we don't use it
 // declare module '*.svg' {
 //   const content: any;
